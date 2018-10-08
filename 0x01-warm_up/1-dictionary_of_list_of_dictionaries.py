b766fc324f6e3a61a61b89d818c62c24a5cf07fe
Mikaela Gurney <354@holbertonschool.com>
2018-10-08 13:09:44 -0700

    Add file export functionality

--- 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -26,22 +26,6 @@ if __name__ == '__main__':
                      "task": task.get('title'),
                      "completed": task.get('completed')})
         newdict[user.get('id')] = task_array
-    print(json.dumps(newdict))
 
-    # tasks = response.json()
-    # employee = user.json()[0].get('username')
-
-    # total = 0
-    # completed = 0
-    # task_list = []
-
-    # for task in tasks:
-    #     if task.get('completed'):
-    #         completed += 1
-    #         task_list.append(task.get('title'))
-    #     total += 1
-
-    # print("Employee {} is done with tasks({}/{}):".
-    #       format(employee, completed, total))
-    # for task in task_list:
-    #     print("\t {}".format(task))
+    with open("todo_all_employees.json", "w") as f:
+        f.write(json.dumps(newdict))
